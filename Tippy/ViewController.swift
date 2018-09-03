ca1619b6804bbd41813bec2514cf5b44626cea56
user910754 <user910754@74-80-245-26.ic2net.net>
2018-09-02 22:39:11 -0700

    completed

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var tipControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +30,12 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     @IBAction func calculateTip(_ sender: Any) {
-        
+        let tipPercentages = [0.18, 0.20, 0.25]
         let bill = Double(billField.text!) ?? 0
-        
-        tipLabel.text = "1"
-        totalLabel.text = "2"
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
     
 }
